d400e66dc9dc09d41a3aa5831367e604ddfdc22e
unknown <pmanonita@FAIHYD03W016.intl.corp.firstam.com>
2015-07-08 23:55:54 +0530

    headers and footers

---
+++ app/scripts/js/footer.js
@@ -0,0 +1,2 @@
+document.write("<div class='navbar navbar-fixed-bottom'><div class='footer'><div class='container'><p>Posted by: Hege Refsnes</p><p>Contact information: <a href='mailto:someone@example.com'>someone@example.com</a>.</p></div></div></div>");
+
