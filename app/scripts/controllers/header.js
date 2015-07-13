edbd719a8839c05e3484eb07894587a415a80953
unknown <praja@HYDWS71072.hyd.corp.int.untd.com>
2015-07-13 18:31:33 +0530

    New changes

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,34 +1,39 @@
-angular.module('lrwebApp')
-  .controller('HeaderCtrl', ['$scope', '$log', '$location', 'userService', function ($scope, $log, $location, userService) {
-    
-     $scope.isLoggedIn = userService.isLoggedIn();
-
-     var user = userService.getUser();    
-     $scope.username = user.firstName || user.name;     
-     console.log("login at header "+$scope.isLoggedIn);
-     console.log("username at header "+$scope.username);
-    
-
-      $scope.$on('user:updated', function() {
-        var u = userService.getUser();
-        $scope.isLoggedIn = u.isLoggedIn;
-        $scope.username = u.firstName || u.name;
-        $scope.profilePic = user.profilePic;
-        $scope.initials = user.initials;
+  angular.module('lrwebApp')
+    .controller('HeaderCtrl', ['$scope', '$log', '$location', 'userService', function ($scope, $log, $location, userService) {
       
-        console.log("login1 at header"+$scope.isLoggedIn);
+       $scope.isLoggedIn = userService.isLoggedIn();
+       $scope.isAdmin = userService.isAdmin();     
+
+       var user = userService.getUser();    
+       $scope.username = user.firstName || user.name;    
+
+        $scope.signout = function(){
+          //signout the user
+          userService.signout().finally(function() {            
+            $log.debug('After logging out ' + JSON.stringify(userService.getUser()));
+            $location.path('/');
+          });
+          return false;
+        };
+
+        $scope.$on('user:updated', function() {
+          var u = userService.getUser();
+          $scope.isLoggedIn = u.isLoggedIn;
+          $scope.username = u.firstName || u.name;
+          $scope.profilePic = user.profilePic;
+          $scope.initials = user.initials;
+
+          $scope.isAdmin = userService.isAdmin();
+      });
+
+     
+    }]);
+            
     
-    	console.log("username at header "+$scope.username);
-    });
+
+
+
+
+
 
    
-  }]);
-          
-  
-
-
-
-
-
-
- 
