cf9f5a0c50f0d8da3b993e3041f5cfbdc5119b8a
rajapattanayak <pattanayak.raja@gmail.com>
2015-08-04 20:14:04 +0530

    multi lr related

---
+++ app/scripts/controllers/searchmultilr.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lrwebApp.controller:SearchMultiLRCtrl
+ * @description
+ * # SearchMultiLRCtrl
+ * Controller of the lrwebApp
+ */
+angular.module('lrwebApp')
+  .controller('SearchMultiLRCtrl',['$scope', '$log', '$location', 'lrService', 'userService',
+    function ($scope, $log, $location, lrService, userService){
+
+    $scope.msg = "";
+    $scope.filter = {};
+
+    $scope.submitForm = function() {
+      
+      $log.debug($scope.filter);
+      $scope.msg = "";
+      
+      lrService.getLRList($scope.filter).then(function(u) {
+        $log.debug('Got LR List'); 
+        $scope.LRList = u;      
+        $scope.msg = "Successfully fetched the Data"
+        
+      }, function(res) {        
+        $log.debug('Issue while getting LR data' + JSON.stringify(res));
+        $scope.LRList = [];
+        $scope.msg = res.msg;
+      });
+      return false;
+    };
+
+  }]);
